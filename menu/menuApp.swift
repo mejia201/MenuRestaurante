a7c37774664ea47ebfe3664141129262ac08a937
mejia201 <bryan.mejia@catolica.edu.sv>
2023-09-23 06:58:39 -0600

    Initial Commit

---
+++ menu/menuApp.swift
@@ -0,0 +1,17 @@
+//
+//  menuApp.swift
+//  menu
+//
+//  Created by Bryan Mejia on 23/9/23.
+//
+
+import SwiftUI
+
+@main
+struct menuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
